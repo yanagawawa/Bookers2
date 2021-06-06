717d854f9f47b82bfe39a0be0e454ad521509106
yanagawawa <yukkebbnba@gmail.com>
2021-06-06 03:57:47 +0000

    sign inページ更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
