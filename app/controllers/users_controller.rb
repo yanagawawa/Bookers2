5a1c96b1ce9de432bec9ddc035133d9205bf9efd
yanagawawa <yukkebbnba@gmail.com>
2021-06-08 12:54:00 +0000

    indexページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def index
-
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
   def edit
     @user = User.find(params[:id])
