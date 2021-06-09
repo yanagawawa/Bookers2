e0d01b33b371bee1087e913164fd01bf5b22d2bb
yanagawawa <yukkebbnba@gmail.com>
2021-06-09 10:26:51 +0000

    フラッシュメッセージ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     if book.update(book_params)
        flash[:notice] = "You have updated book successfully."
-       redirect_to books_path
+       redirect_to book_path(book)
     end
   end
 
