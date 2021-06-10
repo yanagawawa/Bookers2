3e8840785d9ced86312d4a9b3803731988283b3d
yanagawawa <yukkebbnba@gmail.com>
2021-06-10 02:44:18 +0000

    バリデーションname制作

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   attachment :profile_image
+  validates :name, uniqueness: true, length: { in: 2..20 }
 end
