1937e0552139dd40790f8d14c7a19aa00b06e743
yanagawawa <yukkebbnba@gmail.com>
2021-06-10 03:16:01 +0000

    バリデーション実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  validates :title, absence: true
+  validates :body, absence: true, length: { maximum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :title, length: { maximum: 50 }
 end
